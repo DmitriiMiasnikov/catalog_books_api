ae7511cad432cc66d025b27222de11261a1faed1
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2021-02-17 02:26:30 +0300

    fix list of filters by genre

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const config = require('config');
 const usersRoutes = require('./routes/usersRoutes');
 const listRoutes = require('./routes/listRoutes');
 const cors = require('cors');
-// const session = require('express-session');
-// const cookieParser = require('cookie-parser')
-// const MongoStore = require('connect-mongo')(session);
 
 const app = express();
 
@@ -15,19 +12,7 @@ app.use(cors());
 app.options('*', cors());
 app.use('/users', usersRoutes);
 app.use('/list', listRoutes);
-// app.use(cookieParser());
 
-// app.use(session({
-//   secret: 'keyboard cat',
-//   resave: true,
-//   saveUninitialized: true,
-//   store: new MongoStore({ url: config.get('mongoUri') }),
-//   cookie: { maxAge: 60000 }
-// }))
-// app.get('/', function (req, res) {
-//   console.log('session: ', req.session)
-//   console.log('sessionID: ', req.sessionID)
-// })
 const start = async () => {
   try {
     await mongoose.connect(config.get('mongoUri'), {
